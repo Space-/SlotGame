10632ad027c1552cb630679cd3438c8f4dc073db
Bruce <gkm85663@gmail.com>
2019-03-24 21:20:16 +0800

    Add new test - 'Reels_same_order_three_Star_return_90', if three items of reels are 'Star', then score is 90.

--- SlotGame/SlotGameTest.cs
+++ SlotGame/SlotGameTest.cs
@@ -22,8 +22,22 @@ namespace Tests
             var reelItems = new List<ReelItem[]> { reel, reel, reel };
             int[] spinIndexes = { 0, 0, 0 };
 
-            var slotScore = slotMachine.GetSlotScore(reelItems, spinIndexes);
+            var slotScore = slotMachine.GetSlotScore(reelItems, spinIndexes, ReelItem.Wild);
             Assert.AreEqual(100, slotScore);
+        }
+
+        [Test]
+        public void Reels_same_order_three_Star_return_90()
+        {
+            var slotMachine = new SlotMachine();
+
+            var reel = GetReelItems();
+
+            var reelItems = new List<ReelItem[]> { reel, reel, reel };
+            int[] spinIndexes = { 1, 1, 1 };
+
+            var slotScore = slotMachine.GetSlotScore(reelItems, spinIndexes, ReelItem.Star);
+            Assert.AreEqual(90, slotScore);
         }
 
         private static ReelItem[] GetReelItems()
@@ -48,18 +62,30 @@ namespace Tests
 
     public class SlotMachine
     {
-        public int GetSlotScore(List<ReelItem[]> reelItems, int[] spinIndexes)
+        public int GetSlotScore(List<ReelItem[]> reelItems, int[] spinIndexes, ReelItem prizeReelItem)
         {
             for (var reelNum = 0; reelNum < spinIndexes.Length; reelNum++)
             {
                 var spinItemIndex = spinIndexes[reelNum];
-                if (reelItems[reelNum][spinItemIndex] != ReelItem.Wild)
+                if (reelItems[reelNum][spinItemIndex] != prizeReelItem)
                 {
                     return 0;
                 }
             }
 
-            return 100;
+            var slotScore = 0;
+            switch (prizeReelItem)
+            {
+                case ReelItem.Wild:
+                    slotScore = 100;
+                    break;
+
+                case ReelItem.Star:
+                    slotScore = 90;
+                    break;
+            }
+
+            return slotScore;
         }
     }
 }
