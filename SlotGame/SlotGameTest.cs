7ab4ca45352dd56e7729b2ce399a366bc226edb9
Bruce <gkm85663@gmail.com>
2019-03-24 20:27:21 +0800

    Add new test - 'Reels_same_order_return_100' , if three items of reels are 'Wild', then score is 100.

--- SlotGame/SlotGameTest.cs
+++ SlotGame/SlotGameTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace Tests
@@ -10,9 +11,35 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void Reels_same_order_return_100()
         {
-            Assert.Pass();
+            var slotMaching = new SlotMachine();
+
+            string[] reel = { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+
+            var reelItems = new List<string[]> { reel, reel, reel };
+            int[] spinIndexes = { 0, 0, 0 };
+
+            var slotScore = slotMaching.GetSlotScore(reelItems, spinIndexes);
+            Assert.AreEqual(100, slotScore);
+        }
+    }
+
+    public class SlotMachine
+    {
+        //        private int reel1 =
+        public int GetSlotScore(List<string[]> reelItems, int[] spinIndexes)
+        {
+            for (var reelNum = 0; reelNum < spinIndexes.Length; reelNum++)
+            {
+                var spinItemIndex = spinIndexes[reelNum];
+                if (reelItems[reelNum][spinItemIndex] != "Wild")
+                {
+                    return 0;
+                }
+            }
+
+            return 100;
         }
     }
 }
