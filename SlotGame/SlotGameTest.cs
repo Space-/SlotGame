cc03697aac71bcb8dacbeb8d142bda5b2d3c227f
Bruce <gkm85663@gmail.com>
2019-03-24 20:45:44 +0800

    Refactor - create  'GetReelItems' static method to get all items in ReelItem.

--- SlotGame/SlotGameTest.cs
+++ SlotGame/SlotGameTest.cs
@@ -15,15 +15,20 @@ namespace Tests
         [Test]
         public void Reels_same_order_return_100()
         {
-            var slotMaching = new SlotMachine();
+            var slotMachine = new SlotMachine();
 
-            var reel = Enum.GetValues(typeof(ReelItem)).Cast<ReelItem>().ToArray();
+            var reel = GetReelItems();
 
             var reelItems = new List<ReelItem[]> { reel, reel, reel };
             int[] spinIndexes = { 0, 0, 0 };
 
-            var slotScore = slotMaching.GetSlotScore(reelItems, spinIndexes);
+            var slotScore = slotMachine.GetSlotScore(reelItems, spinIndexes);
             Assert.AreEqual(100, slotScore);
+        }
+
+        private static ReelItem[] GetReelItems()
+        {
+            return Enum.GetValues(typeof(ReelItem)).Cast<ReelItem>().ToArray();
         }
     }
 
